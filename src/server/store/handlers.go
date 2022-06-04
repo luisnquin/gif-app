59999a2c3d9b5f4490c2cc755e77570d483ae878
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-06-04 10:14:53 -0500

    feat: automock handler at the point of just capturing stdout

--- src/server/store/handlers.go
+++ src/server/store/handlers.go
@@ -1,7 +1,9 @@
 package store
 
 import (
+	"bufio"
 	"net/http"
+	"os/exec"
 
 	"github.com/luisnquin/meow-app/src/server/log"
 )
@@ -20,5 +22,42 @@ func (m *database) HealthHandler() http.HandlerFunc {
 
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("connection alive"))
+	}
+}
+
+func (m *database) AutoMockHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cmd := exec.CommandContext(r.Context(), "./tools/automock/main.py", "--stdout", "--length=10")
+		pipe, err := cmd.StdoutPipe()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		defer pipe.Close()
+
+		stmts := make([]string, 0)
+
+		s := bufio.NewScanner(pipe)
+		for s.Scan() {
+			stmts = append(stmts, s.Text())
+		}
+
+		err = cmd.Start()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		err = cmd.Wait()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
